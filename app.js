2a91d5d5158328641c3a6a16caa07865bf281e00
Yash Raj <yashrj130@gmail.com>
2019-06-19 15:33:02 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+// variables
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
+
+let cart = [];
+
+document.addEventListener("DOMContentLoaded", () => {
+	console.log("Content Loaded");
+});
