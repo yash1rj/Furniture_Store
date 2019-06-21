27818c265801fc9fafed4987816d89ed1e80b24f
Yash Raj <yashrj130@gmail.com>
2019-06-21 11:37:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,74 @@ const productsDOM = document.querySelector(".products-center");
 
 let cart = [];
 
+// getting the products
+class Products {
+	async getProducts() {
+		try {
+			let result = await fetch("./products.json");
+			let data = await result.json();
+			
+			let products = data.items;
+			products = products.map(item => {
+				const { title, price } = item.fields;
+				const { id } = item.sys;
+				const image = item.fields.image.fields.file.url;
+				return { title, price, id, image };
+			});
+			
+			// console.log(products);
+			return products;
+		}
+		catch(error) {
+			console.log(error);
+		}
+	}
+}
+
+// display products
+class UI {
+	displayProducts(products) {
+		let result = "";
+		products.forEach(product => {
+			result += `
+			<!-- single product -->
+					
+				<article class="product">
+					<div class="img-container">
+						<img src=${product.image} alt="product" class="product-img"/>
+						<button class="bag-btn" data-id=${product.id}>
+							<i class="fas fa-shopping-cart"></i>
+							add to bag
+						</button>
+					</div>
+					<h3>${product.title}</h3>
+					<h4>$${product.price}</h4>
+				</article> 
+				
+			<!-- end of single product -->
+			`;
+		});
+		
+		productsDOM.innerHTML = result;
+	}
+}
+
+// local storage
+class Storage {
+	
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-	console.log("Content Loaded");
+	// console.log("Content Loaded");
+	
+	const ui = new UI();
+	const products = new Products();
+	
+	// get all products
+	products
+		.getProducts()
+		.then(products => {
+			// console.log(products);
+			ui.displayProducts(products);
+		});
 });
